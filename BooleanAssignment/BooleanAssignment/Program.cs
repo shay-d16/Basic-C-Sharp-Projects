2becff704216667812c544b0f39bca7bc5773d73
Shay R <shalondra.davis@protonmail.com>
2021-08-02 17:00:33 -0700

    Boolean Logic Assigment
    Utilize the AND, OR, and XOR operators in C#.

---
+++ BooleanAssignment/BooleanAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace BooleanAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("CAR INSURANCE APPLICATION");
+
+            //AGE 
+            Console.WriteLine("\nWhat is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            // This casts the 'age' variable to an integer that can be read from user input
+            bool legalAge = age >= 16;
+            // This bool ensures that the user is at least 16 years old.
+
+            //DUIs
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false.\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            // This cast creates an input for the user to put in 'true' or 'false'
+            bool noDUIs = dui == false;
+            // This bool ensures that no user with a DUI can be qualified for insurance.
+
+            //TICKETS
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool maxTickets = tickets <= 2;
+            // This bool ensures the user can only qualify if they have 2 or less speeding tickets.
+
+            //QUALIFICATION
+            Console.WriteLine("Qualified?");
+            bool qualified = legalAge && noDUIs && maxTickets;
+            // Utilizing the AND operator, this boolean logic ensures that the user meets all the
+            // requirements for qualification.
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            //AND OPERATOR
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //OR OPERATOR
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //EQUALS OPERATOR
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //DOES NOT EQUAL OPERATOR
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //XOR OPERATOR
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            // The XOR operator will evaluate to 'True' if one of the values is true,
+            // but not both.
+
+            Console.ReadLine();
+        }
+    }
+}
