6125d0d8bb5a863393167e3fe843bd22aee35530
Shay R <shalondra.davis@protonmail.com>
2021-08-04 17:13:53 -0700

    Branching Submission Assignment
    Utilize conditional If/Else statements and learn to use ternary operators.

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express! Please follow the instructions below.");
+
+            Console.WriteLine("\nPlease enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine()); //Cast this variable for user input
+
+
+            if (weight > 50) //Set a condition that restricts packages with a weight greater than 50 pounds
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else //If the package is less than 50 pounds, execute the next part of the program
+            {
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int dimensionsAdd = width + height + length;
+
+                if (dimensionsAdd > 50) //Set this condition for ensure that the items dimensions are not greater than 50
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine(); //If true, print this message and close the program
+                }
+                else //Otherwise use these variables to calculate the 'quote'
+                {
+                    int dimensionsMultiply = width * height * length;
+                    int product = dimensionsMultiply * weight;
+                    int quote = Convert.ToInt32(product / 100);
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + quote + ".00");
+                    Console.WriteLine("Thank you!");
+                }
+            }
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ BranchingLesson/BranchingLesson/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace BranchingLesson
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            //CONDITIONAL STATEMENT 1
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature");
+            //}
+            // This is a lot of code than can be condensed using 'ternary operators.'
+
+            //TERNARY OPERATORS 1
+            //string comparisonResults = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResults);
+
+            //CONDITIONAL STATEMENT 2
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhhh.... something went wrong.");
+            //}
+
+            //TERNARY OPERATORS 2
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string results = favNum == 12 ? "You have an awesome favorite number!" : "You do not have an awesome favorite number.";
+
+            Console.WriteLine(results);
+
+            Console.ReadLine();
+        }
+    }
+}
