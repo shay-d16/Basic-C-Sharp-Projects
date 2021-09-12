0cec9f206a5d359a13e5dff6053441b395e6a290
Shay R <shalondra.davis@protonmail.com>
2021-09-12 15:20:15 -0700

    Submission Assignment
    Create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen. Specify the parameters being passed by name.

---
+++ MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Math
+    {
+        public void Display(int numA, int numB)
+        {
+            int math1 = numA * 10;
+            Console.WriteLine(numB); 
+            //Because we're just displaying the second parameter, we will use Console.WriteLine()
+            //instead of return. Void methods don't return values.
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        //PURPOSE:
+        // 1. Create a class. In that class, create a void method that takes two integers as parameters.
+        // Have the method do a math operation on the first integer and display the second integer to the screen. 
+        // 2. In the Main() method of the console app, instantiate the class .
+        // 3. Call the method in the class, passing in two numbers. 
+        // 4. Call the method in the class, specifying the parameters by name.
+        static void Main(string[] args)
+        {
+            Math math = new Math();//Here we instantiate the Math class as a 'math' object
+            math.Display(numA:2, numB:4);
+            //Here, we've named the parameters being passed to the void 'Display' method.
+            //The name of the parameter and the value are separated by a colon ":"
+            Console.ReadLine();
+        }
+    }
+}
