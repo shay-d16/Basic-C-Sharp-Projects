f376c1a9e92e344097a6821736c52d6c20870b87
Shay R <shalondra.davis@protonmail.com>
2021-12-05 18:53:50 -0800

    MVC Applications
    Create basic MVC applications.

---
+++ MVCApp/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Shay";
+            user.LastName = "Davis";
+            user.Age = 26;
+
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hello!";
+
+            return View();
+        }
+    }
+}
