d1a1ce5a501e6aa74fe9d5ceefe97d1f50762ca8
Shay R <shalondra.davis@protonmail.com>
2021-12-08 18:09:31 -0800

    Part 10
    Create an Admin page that allows the administrator to view information from the database.

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using NewsletterAppMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,18 @@ namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
+        // The first thing you do with ADO.NET before you can access any information inside of a 
+        // database is create a connection string (a string of characters within quotations that contains
+        // specific information that allows access to the database like the server name or credential info, etc.)
+        // The best way to get a connection string for ADO.NET is to connect to the database inside
+        // of Visual Studio.
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;
+                                            Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                            TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                            MultiSubnetFailover=False";
+        // We used the 'readonly' keyword here to protect the connection string from changing anywhere in our code.
+
+
         public ActionResult Index()
         {
             return View();
@@ -15,7 +30,7 @@ namespace NewsletterAppMVC.Controllers
 
         [HttpPost]
         public ActionResult SignUp(string firstName, string lastName, string emailAddress)
-        {
+        {                                             
             if(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
                 // This checks if any of the variables are an empty string or have a 'null' value.
             {
@@ -28,31 +43,74 @@ namespace NewsletterAppMVC.Controllers
             else
             // This else statement will save the data submitted to our local database using ADO.NET
             {
-                // The first thing you do with ADO.NET before you can access any information inside of a 
-                // database is create a connection string (a string of characters within quotations that contains
-                // specific information that allows access to the database like the server name or credential info, etc.)
-                // The best way to get a connection string for ADO.NET is to connect to the database inside
-                // of Visual Studio.
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;
-                                            Integrated Security=True;Connect Timeout=30;Encrypt=False;
-                                            TrustServerCertificate=False;ApplicationIntent=ReadWrite;
-                                            MultiSubnetFailover=False";
+                
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+                // Remember, having parameters like this instead of "raw" user input helps prevent SQL Injection.
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) //passing in 'connectionString'
+                    // Remember, any time you're opening up an outside connection, i.e. to a db, you always want to wrap
+                    // it in a 'using' statement b/c that when you are done with it, it gets cut off or closed. Leaving 
+                    // it open can cause memory leaks or slowdowns in your program later.
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    //Now that we've added the parameters, we need to give them values:
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                } // This whole 'using' statement should 'INSERT' whatever the user puts into the form into the db.
+
                 return View("Success");
             }
         }
 
-        public ActionResult About()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your application description page.";
+            // Here, we'll create logic for pulling the information from the database and display it to the
+            // admin, which we will do using ADO.NET
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress from SignUps";
 
-            return View();
-        }
+            // Now we'll create a list of our model
+            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+            // This instantiates a new, empty list called 'signups'
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            // Next, we'll open a connection and wrap it in a 'using' statement.
+            using (SqlConnection connection = new SqlConnection(connectionString)) 
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
 
-            return View();
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // While this information is being read, it will also be assigned the values
+                    // coming fromt the database.
+
+                    // When creating a new object, it is not necessary to explicitly state the data
+                    // type if the data type is obvious.
+                    var signup = new NewsletterSignUp();
+
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    // We'll add this to the list we just created:
+                    signups.Add(signup);
+                }
+            }
+            // So we have populated a list of 'NewsletterSignUps' (which is our model or Business object), 
+            // and the list is called 'signups'. Now, we will pass this list to the 'View'.
+            return View(signups);
         }
     }
 }
---
+++ NewsletterAppMVC/NewsletterAppMVC/Models/NewsletterSignUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsletterAppMVC.Models
+{
+    // Remember, a 'Model' is just a simple class, and it is always singular; It's a Business object. If 
+    // the object was going to be plural, then it would be a list of them.
+    public class NewsletterSignUp
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
