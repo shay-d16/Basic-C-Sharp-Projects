76050eba13c62daa30d708ad1388a1103a333fdb
Shay R <shalondra.davis@protonmail.com>
2021-12-10 16:19:25 -0800

    Part 3
    Replace the ADO.NET code with Entity Framework syntax.

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -44,30 +44,40 @@ namespace NewsletterAppMVC.Controllers
             else
             // This else statement will save the data submitted to our local database using ADO.NET
             {
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
                 
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                                        (@FirstName, @LastName, @EmailAddress)";
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                //                        (@FirstName, @LastName, @EmailAddress)";
                 // Remember, having parameters like this instead of "raw" user input helps prevent SQL Injection.
 
-                using (SqlConnection connection = new SqlConnection(connectionString)) //passing in 'connectionString'
+                //using (SqlConnection connection = new SqlConnection(connectionString)) //passing in 'connectionString'
                     // Remember, any time you're opening up an outside connection, i.e. to a db, you always want to wrap
                     // it in a 'using' statement b/c that when you are done with it, it gets cut off or closed. Leaving 
                     // it open can cause memory leaks or slowdowns in your program later.
-                {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
 
-                    //Now that we've added the parameters, we need to give them values:
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
+                //    //Now that we've added the parameters, we need to give them values:
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
 
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
-                } // This whole 'using' statement should 'INSERT' whatever the user puts into the form into the db.
+                //    connection.Open();
+                //    command.ExecuteNonQuery();
+                //    connection.Close();
+                //} // This whole 'using' statement should 'INSERT' whatever the user puts into the form into the db.
 
                 return View("Success");
             }
@@ -75,69 +85,72 @@ namespace NewsletterAppMVC.Controllers
 
         public ActionResult Admin()
         {
+            using(NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps;
+                // A common practice on the controller is to map your database object to a view model. So let's create an empty
+                //  list with the 'SignupVm' data type: 
+                var signupVms = new List<SignupVm>();
+                // The data type is obvious here so it's best practice to use the 'var' keyword in this instance.
+
+                // Then we will loop through all the 'signups':
+                foreach (var signup in signups)
+                {
+                    // Here, we will map properties between objects
+                    var signupVm = new SignupVm();
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+
             // Here, we'll create logic for pulling the information from the database and display it to the
             // admin, which we will do using ADO.NET
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
+            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
 
             // Now we'll create a list of our model
-            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+            //List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
             // This instantiates a new, empty list called 'signups'
 
             // Next, we'll open a connection and wrap it in a 'using' statement.
-            using (SqlConnection connection = new SqlConnection(connectionString)) 
-            {
-                SqlCommand command = new SqlCommand(queryString, connection);
+            //using (SqlConnection connection = new SqlConnection(connectionString)) 
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection);
 
-                connection.Open();
+            //    connection.Open();
 
-                SqlDataReader reader = command.ExecuteReader();
+            //    SqlDataReader reader = command.ExecuteReader();
 
-                while (reader.Read())
-                {
+            //    while (reader.Read())
+            //    {
                     // While this information is being read, it will also be assigned the values
                     // coming fromt the database.
 
                     // When creating a new object, it is not necessary to explicitly state the data
                     // type if the data type is obvious.
-                    var signup = new NewsletterSignUp();
+                    //var signup = new NewsletterSignUp();
 
-                    signup.Id = Convert.ToInt32(reader["Id"]);
-                    signup.FirstName = reader["FirstName"].ToString();
-                    signup.LastName = reader["LastName"].ToString();
-                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    //signup.Id = Convert.ToInt32(reader["Id"]);
+                    //signup.FirstName = reader["FirstName"].ToString();
+                    //signup.LastName = reader["LastName"].ToString();
+                    //signup.EmailAddress = reader["EmailAddress"].ToString();
                     
                     // B/c we don't want to display something like a user's social to the 'View' we need a way to return
                     // these objects without the 'SocialSecurityNumber' object specifically.
                     // This can be done using a View Model, which is a model that is stripped down to ONLY what you need for
                     // the 'View'. View Models live in a separate folder called 'ViewModels' so we'll need to create that. 
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();                                       
+                    //signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();                                       
                     
                     // We'll add all of this to the list we just created:
-                    signups.Add(signup);
-                }
-            }
-            // A common practice on the controller is to map your database object to a view model. So let's create an empty
-            //  list with the 'SignupVm' data type: 
-            var signupVms = new List<SignupVm>();
-            // The data type is obvious here so it's best practice to use the 'var' keyword in this instance.
-
-            // Then we will loop through all the 'signups':
-            foreach (var signup in signups)
-            {
-                // Here, we will map properties between objects
-                var signupVm = new SignupVm();
-                signupVm.FirstName = signup.FirstName;
-                signupVm.LastName = signup.LastName;
-                signupVm.EmailAddress = signup.EmailAddress;
-                signupVms.Add(signupVm);
-            }
-
-
-
+            //        signups.Add(signup);
+            //    }
+            //}            
 
             // So we have populated a list of 'NewsletterSignUps' (which is our model or Business object), 
             // and the list is called 'signups'. Now, we will pass this list to the 'View'.
-            return View(signupVms);
+            
         }
     }
 }
