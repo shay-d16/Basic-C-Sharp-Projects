67c91f9b7b275ca508b80823c2b1b7ae6cc174a0
Shay R <shalondra.davis@protonmail.com>
2021-12-19 15:22:00 -0800

    More Challenges

---
+++ FinalChallengeAssignment1/FinalChallengeAssignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallengeAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Shay" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
